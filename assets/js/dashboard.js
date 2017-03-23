09e2c9ec0749d376ed1e8079e2a9ec015bf438b2
Trevor Rudolph <trevor@trevor.la>
2017-03-23 07:06:10 -0400

    Frontend fixes and start OAuth for Slack
    Shaded circles / Port 80 / Slack

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -7,16 +7,17 @@ let init = () => {
   channel.on("connect", light => {
     let color = light["color"]
     let $list = $(".lights")
-    let $newLight = $(lightElement(light["slack_handle"], light["color"]))
+    let $newLight = $(lightElement(light["slack_handle"]))
     $newLight.attr("data-uid", light["uid"])
-
+    $newLight.css({"background": `radial-gradient(circle at 100px 100px, ${light["color"]}, ${nameShadow(light["color"])})`})
     $list.append($newLight)
+    $newLight.textfill();
     console.log(light)
   })
 
   channel.on("change", light => {
     let $li = $(`li[data-uid='${light["uid"]}']`)
-    $li.css({"backgroundColor": light["color"]})
+    $li.css({"background": `radial-gradient(circle at 100px 100px, ${light["color"]}, ${nameShadow(light["color"])})`})
     console.log(light)
   })
 
@@ -25,10 +26,31 @@ let init = () => {
     $li.remove()
     console.log(light)
   })
+
+  $.fn.textfill = function(options) {
+    return this.each(function(){
+      var textDiv = this;
+      var textSpan = $(this).find("p")[0];
+      console.log(textDiv, textSpan);
+
+      if (textSpan.getClientRects()[0].width < textDiv.getClientRects()[0].width)
+        return;
+
+      textSpan.style.fontSize = '64px';
+
+      while(textSpan.getClientRects()[0].width > textDiv.getClientRects()[0].width)
+      {
+        textSpan.style.fontSize = (parseInt(textSpan.style.fontSize) - 1)+"px";
+      }
+    });
+  }
+
+  $(".light").textfill();
+
 }
 
-let lightElement = (slack_handle, color) => (
-  `<li class='light' style="background-color:${color}">
+let lightElement = (slack_handle) => (
+  `<li class='light'>
     <div class='outer-div'>
       <div class='username'>
         <p>${slack_handle.toUpperCase()}</p>
@@ -37,6 +59,11 @@ let lightElement = (slack_handle, color) => (
   </li>`
 )
 
-
+let nameShadow = (hex) => {
+  console.log("tits!");
+  let c = hex.substring(1).split('')
+  c = '0x' + c.join('');
+  return 'rgb('+[(c>>16)&160, (c>>8)&160, c&160].join(',')+')';
+}
 
 init()
--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :zen_sky_board, ZenSkyBoard.Web.Endpoint,
-  http: [port: 4000],
+  http: [port: 80],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
